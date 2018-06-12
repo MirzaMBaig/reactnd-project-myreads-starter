3061247c487d0f2e820d99604cdd0462e854bb52
mirza.baig <mirza.baig@dwp.gsi.gov.uk>
2018-06-12 12:36:33 +0100

    Update component to stateless

--- src/BookComponent.js
+++ src/BookComponent.js
@@ -2,35 +2,31 @@
  * Created by mmab on 06/03/2018.
  */
 
-import React, {Component} from "react";
+import React from "react";
 
-class BookComponent extends Component{
-
-    render(){
-        let book = this.props.book;
-        return  <li>
-            <div className="book">
-                <div className="book-top">
-                    <div className="book-cover" style={{
-                        width: 128,
-                        height: 193,
-                        backgroundImage: `url(${book.imageLinks.thumbnail})`
-                    }}></div>
-                    <div className="book-shelf-changer">
-                        <select value={book.shelf?book.shelf:'none'} onChange={(event)=> this.props.updateBookShelf && this.props.updateBookShelf(book, event.target.value) }>
-                            <option value="move" disabled>Move to...</option>
-                            <option value="currentlyReading">Currently Reading</option>
-                            <option value="wantToRead">Want to Read</option>
-                            <option value="read">Read</option>
-                            <option value="none">None</option>
-                        </select>
-                    </div>
+export default function BookComponent(props) {
+    let book = props.book;
+    return <li>
+        <div className="book">
+            <div className="book-top">
+                <div className="book-cover" style={{
+                    width: 128,
+                    height: 193,
+                    backgroundImage: `url(${book.imageLinks.thumbnail})`
+                }}></div>
+                <div className="book-shelf-changer">
+                    <select value={book.shelf ? book.shelf : 'none'} onChange={(event) => props.updateBookShelf && props.updateBookShelf(book, event.target.value)}>
+                        <option value="move" disabled>Move to...</option>
+                        <option value="currentlyReading">Currently Reading</option>
+                        <option value="wantToRead">Want to Read</option>
+                        <option value="read">Read</option>
+                        <option value="none">None</option>
+                    </select>
                 </div>
-                <div className="book-title">{book.title}</div>
-                <div className="book-authors">{book.authors && book.authors.map(author => <span key={author}>{author}</span> )}</div>
             </div>
-        </li>
-    }
+            <div className="book-title">{book.title}</div>
+            <div className="book-authors">{book.authors && book.authors.map(author => <span key={author}>{author}</span>)}</div>
+        </div>
+    </li>
 }
 
-export default BookComponent;
